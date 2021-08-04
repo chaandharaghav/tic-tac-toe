82bf61d9e0fbdf9fea7d805a08b258868bf8c80a
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2021-08-04 13:07:16 +0530

    Add function to update display when clicked on the div and adjust size of board

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const gameBoard = (function () {
   const board = [
-    ["x", "o", "x"],
-    ["o", "", "x"],
-    ["x", "o", "x"],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
 
   const getValue = function (row, col) {
     return board[row][col];
   };
+  const setValue = function (row, col, value) {
+    board[row][col] = value;
+  };
 
-  return { getValue };
+  return { getValue, setValue };
 })();
 
 const displayController = (function () {
   const cells = document.querySelectorAll(".board > *");
   for (const cell of cells) {
-    cell.innerText = gameBoard.getValue(cell.dataset.row, cell.dataset.col);
+    cell.addEventListener("click", function name() {
+      const row = this.dataset.row;
+      const col = this.dataset.col;
+      updateGameBoard(row, col);
+      updateDisplay(this, row, col);
+    });
   }
+
+  let currentValue = "x";
+
+  const updateGameBoard = function (row, col) {
+    if (gameBoard.getValue(row, col) === "") {
+      gameBoard.setValue(row, col, currentValue);
+      currentValue = currentValue === "x" ? "o" : "x";
+    }
+  };
+
+  const updateDisplay = function (cell, row, col) {
+    cell.innerText = gameBoard.getValue(row, col);
+  };
 })();
